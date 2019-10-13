038a1e18c0a7873cc7844ce4bf6e987baac53735
Fagun <mdabuzaforfagun@gmail.com>
2019-10-13 11:35:51 +0600

    Add NullCoalescingAssignments details

---
+++ C Sharp 8 New Features/NullCoalescingAssignments.cs
@@ -0,0 +1,26 @@
+﻿namespace C_Sharp_8_New_Features
+{
+    public class NullCoalescingAssignments
+    {
+        public string CheckNullUsingConditions(string value)
+        {
+            if (value == null)
+            {
+                value = "Batman";
+            }
+            return value;
+        }
+
+        public string CheckNullUsingNullCoalescingOperator(string value)
+        {
+            value = value ?? "Batman";
+            return value;
+        }
+
+        public string CheckNullUsingNullCoalescingAssignmentOperator(string value)
+        {
+            value ??= "Batman";
+            return value;
+        }
+    }
+}
